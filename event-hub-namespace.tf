ff74bb399dcd7675229b8dd153456eb25b07e6f8
Ryan Faircloth <ryan@dss-i.com>
2023-08-16 14:00:50 -0400

    fix: allow disable public ip

--- event-hub-namespace.tf
+++ event-hub-namespace.tf
@@ -1,15 +1,15 @@
 resource "azurerm_eventhub_namespace" "evh" {
-  name                     = var.event_hub_namespace_name
-  location                 = var.location
-  resource_group_name      = var.rg_name
-  sku                      = var.settings.sku
-  capacity                 = try(var.settings.capacity, null)
-  tags                     = var.tags
-  auto_inflate_enabled     = try(var.settings.auto_inflate_enabled, null)
-  dedicated_cluster_id     = try(var.settings.dedicated_cluster_id, null)
-  maximum_throughput_units = try(var.settings.maximum_throughput_units, null)
-  zone_redundant           = try(var.settings.zone_redundant, null)
-  public_network_access_enabled  = var.public_network_access_enabled
+  name                          = var.event_hub_namespace_name
+  location                      = var.location
+  resource_group_name           = var.rg_name
+  sku                           = var.settings.sku
+  capacity                      = try(var.settings.capacity, null)
+  tags                          = var.tags
+  auto_inflate_enabled          = try(var.settings.auto_inflate_enabled, null)
+  dedicated_cluster_id          = try(var.settings.dedicated_cluster_id, null)
+  maximum_throughput_units      = try(var.settings.maximum_throughput_units, null)
+  zone_redundant                = try(var.settings.zone_redundant, null)
+  public_network_access_enabled = var.public_network_access_enabled
 
   dynamic "identity" {
     for_each = length(var.identity_ids) == 0 && var.identity_type == "SystemAssigned" ? [var.identity_type] : []
@@ -30,6 +30,7 @@ resource "azurerm_eventhub_namespace" "evh" {
     for_each = lookup(var.settings, "network_rulesets", {}) != {} ? [1] : []
     content {
       default_action                 = lookup(var.settings.network_rulesets, "default_action", null)
+      public_network_access_enabled  = var.public_network_access_enabled
       trusted_service_access_enabled = lookup(var.settings.network_rulesets, "trusted_service_access_enabled", true)
       dynamic "virtual_network_rule" {
         for_each = lookup(var.settings.network_rulesets, "virtual_network_rule", {}) != {} ? [1] : []
